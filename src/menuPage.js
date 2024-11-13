2ea38b07919bf95e71e39890c566b5c06f270713
Sergio Joya <sejoya104@gmail.com>
2024-11-13 13:44:51 -0500

    Refactor menuPage script

--- src/menuPage.js
+++ src/menuPage.js
@@ -7,7 +7,7 @@ import pastaImage from './images/pasta.jpg';
 import seaFoodImage from './images/seafood.jpg';
 import sandwichImage from './images/sandwich.jpg';
 import dessertImage from './images/dessert.jpeg';
-import drinkImage from './images/wine.png';
+import drinkImage from './images/drink.png';
 
 function createImage(src, alt) {
     const img = document.createElement('img');
@@ -26,41 +26,41 @@ function createCard(title, src, alt) {
 }
 
 export function loadMenuPage() {
-    const menuPageContainer = document.createElement('div');
-    menuPageContainer.id = 'menu-content';
-
-    const img = createImage(menuBackgroundImage, "Menu background image");
-    let div = document.createElement('div');
+    const backgroundImage = createImage(menuBackgroundImage, 
+        "Menu background image");
 
     const h1 = document.createElement('h1');
     h1.textContent = 'Our Menu';
 
     let para = document.createElement('p');
-    para.textContent = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus eveniet, blanditiis neque adipisci expedita iusto assumenda facere ipsam aliquam accusamus';
+    para.textContent = `Lorem ipsum dolor sit, amet consectetur adipisicing 
+        elit. Ducimus eveniet, blanditiis neque adipisci expedita iusto 
+        assumenda facere ipsam aliquam accusamus`;
 
+    const div = document.createElement('div');
     div.append(h1, para);
 
-    const heroContainer = document.createElement('div');
-    heroContainer.classList.add('hero-container');
-    heroContainer.append(img, div);
-
-    const appetizer = createCard('Appetizers', appetizerImage, 'Appetizer');
-    const salad = createCard('Salads', saladImage, 'Salad');
-    const soup = createCard('Soups', soupImage, 'Soup');
-    const pizza = createCard('Pizza', pizzaImage, 'Pizza');
-    const pasta = createCard('Pasta', pastaImage, 'Pasta');
-    const seaFood = createCard('Seafood', seaFoodImage, 'Seafood');
-    const sandwich = createCard('Sandwiches', sandwichImage, 'Sandwich');
-    const dessert = createCard('Desserts', dessertImage, 'Dessert');
-    const drink = createCard('Drinks', drinkImage, 'Drinks');
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('title-container');
+    titleContainer.append(backgroundImage, div);
 
     const cardContainer = document.createElement('div');
     cardContainer.classList = 'card-container';
-    cardContainer.append(appetizer, salad, soup, pizza, pasta, seaFood, sandwich,
-        dessert, drink);
+    cardContainer.append(
+        createCard('Appetizers', appetizerImage, 'Appetizer'),
+        createCard('Salads', saladImage, 'Salad'),
+        createCard('Soups', soupImage, 'Soup'),
+        createCard('Pizza', pizzaImage, 'Pizza'),
+        createCard('Pasta', pastaImage, 'Pasta'),
+        createCard('Seafood', seaFoodImage, 'Seafood'),
+        createCard('Sandwiches', sandwichImage, 'Sandwich'),
+        createCard('Desserts', dessertImage, 'Dessert'),
+        createCard('Drinks', drinkImage, 'Drinks')
+    );
     
-    menuPageContainer.append(heroContainer, cardContainer);
+    const menuPageContent = document.createElement('div');
+    menuPageContent.id = 'menu-content';
+    menuPageContent.append(titleContainer, cardContainer);
 
-    const content = document.querySelector('#content');
-    content.append(menuPageContainer);
+    document.querySelector('#content').append(menuPageContent);
 }

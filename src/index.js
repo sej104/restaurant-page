e837758808f108638c75eb4f8b45c62eb45a2456
Sergio Joya <sejoya104@gmail.com>
2024-11-13 09:46:03 -0500

    Add tab-switching logic to entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import { loadHomePage } from './homePage.js';
 import {loadMenuPage} from './menuPage.js';
+import { loadContactPage } from './contactPage.js';
 import './styles.css';
 
-loadMenuPage();
+loadHomePage();
+
+const content = document.querySelector('#content');
+const homeButton = document.querySelector('#home-btn');
+const menuButton = document.querySelector('#menu-btn');
+const contactButton = document.querySelector('#contact-btn');
+
+homeButton.addEventListener('click', () => {
+    content.replaceChildren();
+    loadHomePage();
+});
+
+menuButton.addEventListener('click', () => {
+    content.replaceChildren();
+    loadMenuPage();
+});
+
+contactButton.addEventListener('click', () => {
+    content.replaceChildren();
+    loadContactPage();
+});
 

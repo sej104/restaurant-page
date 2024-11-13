dcc9b7164e41329193187290d63809865c1623c1
Sergio Joya <sejoya104@gmail.com>
2024-11-13 13:44:25 -0500

    Refactor homePage script

--- src/homePage.js
+++ src/homePage.js
@@ -1,14 +1,9 @@
 import homeBackgroundImage from './images/home-background.jpg';
 
 export function loadHomePage() {
-    const homePageContainer = document.createElement('div');
-    homePageContainer.id = 'home-content';
-
     const img = document.createElement('img');
     img.src = homeBackgroundImage;
     img.alt = 'Homepage background image';
-    
-    const div = document.createElement('div');
     
     const h2 = document.createElement('h2');
     h2.textContent = 'Sapore di Italia';
@@ -17,15 +12,20 @@ export function loadHomePage() {
     h1.textContent = 'The Best Tasting Experience!';
     
     const para = document.createElement('p');
-    para.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint pariatur nostrum modi illum nesciunt! Voluptate, expedita animi dignissimos!';
+    para.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Sint pariatur nostrum modi illum nesciunt! Voluptate, expedita 
+        animi dignissimos!`;
     
     const button = document.createElement('button');
-    button.textContent = 'View Our Menu';
+    button.textContent = 'Make a Reservation';
     
+    const div = document.createElement('div');
     div.append(h2, h1, para, button);
-    homePageContainer.append(img, div);
 
-    const content = document.querySelector("#content");
-    content.append(homePageContainer);
+    const homePageContent = document.createElement('div');
+    homePageContent.id = 'home-content';
+    homePageContent.append(img, div);
+
+    document.querySelector("#content").append(homePageContent);
 }
 
